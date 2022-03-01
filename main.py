beb470d695c56e7f0b1f6ab1d91af5fd5cf361f0
jamesfrys <wnhs615@gmail.com>
2022-02-28 22:35:33 -0500

    removed code that deletes file upon init run

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ pdf.set_font("Arial", size=15)
 txtFile = "/Users/jamesfrys/Documents/CS 361/video_transcriber/lib/transcribed_file.txt"
 pdfFile = "/Users/jamesfrys/Documents/CS 361/video_transcriber/lib/output.pdf"
 
-txtPath = Path(txtFile)
 pdfPath = Path(pdfFile)
-
-if txtPath.is_file():
-    file = open(txtFile, "w")
-    file.close()
 
 if pdfPath.is_file():
     os.remove(pdfFile)
 
 cur = 0
-while True:
+while cur == 0:
     try:
         with open(txtFile) as f:
             contents = f.readlines()
@@ -31,8 +26,8 @@ while True:
                 pdf.cell(200, 10, txt=line, ln=2, align='C')
             for line in contents:
                 pdf.cell(0, 0, txt='', ln=0, align='C')
-
                 pdf.output(pdfFile)
+                cur += 1
     except IOError as e:
         pass
     time.sleep(1)
